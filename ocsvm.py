2ab89f0d3f3bdd51c66f9c7755a79a24bcb76cc4
henry8527 <henrychen8527@gmail.com>
2018-03-10 18:05:37 +0800

    update training data for OCSVM + RF

--- ocsvm.py
+++ ocsvm.py
@@ -3,15 +3,16 @@ import pandas as pd
 import math
 import matplotlib.pyplot as plt
 from sklearn import svm
+import timeit
 
 print("start")
+
+timer_start = timeit.default_timer()
+
 MAX_TIME_INTERVAL = 8353
 
 df = pd.read_csv('/Users/chenhaoyun/Desktop/Y_label/machine_label_Y-500.csv')
-
-# Random X features to test one-class svm
-df['average'] = np.random.randint(100, size=4177000)
-df['peak'] = np.random.randint(100, size=4177000)
+X_df = pd.read_csv('/Users/chenhaoyun/Desktop/X_label/machine_label_X-500.csv')
 
 # Generate training data
 machine_ID = df['machine ID'].unique()
@@ -21,15 +22,55 @@ for ID in machine_ID:
     for time in range(MAX_TIME_INTERVAL + 1 - 6):
         true_time = time + (ID_counter * 8354)
         if true_time == 0:
-            total_row = df['average'].iloc[
+            total_row = X_df['max CPU usage'].iloc[
                 true_time:true_time + 6].values.tolist()
+            total_row.extend(X_df['mean CPU usage'].iloc[
+                             true_time:true_time + 6].values.tolist())
             total_row.extend(
-                df['peak'].iloc[true_time:true_time + 6].values.tolist())
+                X_df['max disk I/O'].iloc[true_time:true_time + 6].values.tolist())
+            total_row.extend(
+                X_df['mean disk I/O'].iloc[true_time:true_time + 6].values.tolist())
+            total_row.extend(X_df['max disk space'].iloc[
+                             true_time:true_time + 6].values.tolist())
+            total_row.extend(X_df['mean disk space'].iloc[
+                             true_time:true_time + 6].values.tolist())
+            total_row.extend(X_df['max memory usage'].iloc[
+                             true_time:true_time + 6].values.tolist())
+            total_row.extend(X_df['mean memory usage'].iloc[
+                             true_time:true_time + 6].values.tolist())
+            total_row.extend(X_df['max page cache'].iloc[
+                             true_time:true_time + 6].values.tolist())
+            total_row.extend(X_df['mean page cache'].iloc[
+                             true_time:true_time + 6].values.tolist())
+            total_row.extend(X_df['max MAI'].iloc[
+                             true_time:true_time + 6].values.tolist())
+            total_row.extend(X_df['mean MAI'].iloc[
+                             true_time:true_time + 6].values.tolist())
             total_row.append(df['Y label'].iloc[true_time + 6])
         else:
-            tmp_row = df['average'].iloc[
+            tmp_row = X_df['max CPU usage'].iloc[
                 true_time:true_time + 6].values.tolist()
-            tmp_row.extend(df['peak'].iloc[
+            tmp_row.extend(X_df['mean CPU usage'].iloc[
+                           true_time:true_time + 6].values.tolist())
+            tmp_row.extend(
+                X_df['max disk I/O'].iloc[true_time:true_time + 6].values.tolist())
+            tmp_row.extend(
+                X_df['mean disk I/O'].iloc[true_time:true_time + 6].values.tolist())
+            tmp_row.extend(X_df['max disk space'].iloc[
+                           true_time:true_time + 6].values.tolist())
+            tmp_row.extend(X_df['mean disk space'].iloc[
+                           true_time:true_time + 6].values.tolist())
+            tmp_row.extend(X_df['max memory usage'].iloc[
+                           true_time:true_time + 6].values.tolist())
+            tmp_row.extend(X_df['mean memory usage'].iloc[
+                           true_time:true_time + 6].values.tolist())
+            tmp_row.extend(X_df['max page cache'].iloc[
+                           true_time:true_time + 6].values.tolist())
+            tmp_row.extend(X_df['mean page cache'].iloc[
+                           true_time:true_time + 6].values.tolist())
+            tmp_row.extend(X_df['max MAI'].iloc[
+                           true_time:true_time + 6].values.tolist())
+            tmp_row.extend(X_df['mean MAI'].iloc[
                            true_time:true_time + 6].values.tolist())
             tmp_row.append(df['Y label'].iloc[true_time + 6])
             total_row.extend(tmp_row)
@@ -37,13 +78,54 @@ for ID in machine_ID:
     ID_counter += 1
 
 total_row = np.array(total_row)
-total_row = total_row.reshape(-1, 13)
-training_df = pd.DataFrame(total_row, columns=['avg1', 'avg2',
-                                               'avg3', 'avg4',
-                                               'avg25', 'avg6',
-                                               'peak1', 'peak2',
-                                               'peak3', 'peak4',
-                                               'peak5', 'peak6', 'Y_label'])
+total_row = total_row.reshape(-1, 73)
+training_df = pd.DataFrame(total_row, columns=['max CPU usage 1', 'max CPU usage 2',
+                                               'max CPU usage 3', 'max CPU usage 4',
+                                               'max CPU usage 5', 'max CPU usage 6',
+                                               'mean CPU usage 1', 'mean CPU usage 2',
+                                               'mean CPU usage 3', 'mean CPU usage 4',
+                                               'mean CPU usage 5', 'mean CPU usage 6',
+                                               'max disk I/O 1', 'max disk I/O 2',
+                                               'max disk I/O 3', 'max disk I/O 4',
+                                               'max disk I/O 5', 'max disk I/O 6',
+                                               'mean disk I/O 1', 'mean disk I/O 2',
+                                               'mean disk I/O 3', 'mean disk I/O 4',
+                                               'mean disk I/O 5', 'mean disk I/O 6',
+                                               'max disk space 1', 'max disk space 2',
+                                               'max disk space 3', 'max disk space 4',
+                                               'max disk space 5', 'max disk space 6',
+                                               'mean disk space 1', 'mean disk space 2',
+                                               'mean disk space 3', 'mean disk space 4',
+                                               'mean disk space 5', 'mean disk space 6',
+                                               'max memory usage 1', 'max memory usage 2',
+                                               'max memory usage 3', 'max memory usage 4',
+                                               'max memory usage 5', 'max memory usage 6',
+                                               'mean memory usage 1', 'mean memory usage 2',
+                                               'mean memory usage 3', 'mean memory usage 4',
+                                               'mean memory usage 5', 'mean memory usage 6',
+                                               'max page cache 1', 'max page cache 2',
+                                               'max page cache 3', 'max page cache 4',
+                                               'max page cache 5', 'max page cache 6',
+                                               'mean page cache 1', 'mean page cache 2',
+                                               'mean page cache 3', 'mean page cache 4',
+                                               'mean page cache 5', 'mean page cache 6',
+                                               'max MAI 1', 'max MAI 2',
+                                               'max MAI 3', 'max MAI 4',
+                                               'max MAI 5', 'max MAI 6',
+                                               'mean MAI 1', 'mean MAI 2',
+                                               'mean MAI 3', 'mean MAI 4',
+                                               'mean MAI 5', 'mean MAI 6',
+                                               'Y_label'])
+
+# drop off ['Y_label'] == -1
+training_df = training_df[training_df['Y_label'] != -1]
+
+training_df.to_csv(
+    '/Users/chenhaoyun/Desktop/machine_label_Training-500.csv', encoding='utf-8', index=False)
+
+timer_end = timeit.default_timer()
+print(timer_end - timer_start)
+# @ timeit single .csv generate features : 4206s
 
 
 X = training_df
